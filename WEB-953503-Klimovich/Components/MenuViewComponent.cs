33ac2b1d33c5d247a67ad7292a0225fce1f0c3bb
artegful <artegful@users.noreply.github.com>
2021-11-17 14:22:59 +0300

    add lab3

---
+++ WEB-953503-Klimovich/Components/MenuViewComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using WEB_953503_Klimovich.Models;
+
+namespace WEB_953503_Klimovich.Components
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private List<MenuItem> menuItems;
+
+        public MenuViewComponent()
+        {
+            menuItems = new List<MenuItem>()
+            {
+                new MenuItem() {Controller = "Home", Action = "Index", Text = "Lab3"},
+                new MenuItem() {Controller = "Product", Action = "Index", Text = "Каталог"},
+                new MenuItem() {IsPage = true, Area = "Admin", Page = "/Index", Text = "Администрирование"}
+            };
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var controller = ViewContext.RouteData.Values["controller"];
+            var area = ViewContext.RouteData.Values["area"];
+
+            foreach (var item in menuItems)
+            {
+                bool isMatchingController = controller?.Equals(item.Controller) ?? false;
+                bool isMatchingArea = area?.Equals(item.Area) ?? false;
+
+                item.IsActive = isMatchingController || isMatchingArea;
+            }
+            return View(menuItems);
+        }
+    }
+}
